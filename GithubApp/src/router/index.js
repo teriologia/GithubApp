a3dd2d5ef587dec03e5945e183da0f7fc7584d3c
mehmet Serdar Tekin <serdar.tekin@orginoacademy.com>
2019-05-12 10:57:36 +0300

    Basic folder system

---
+++ GithubApp/src/router/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Scene, Router, Stack, Actions } from 'react-native-router-flux';
+import { Home } from '../screens'
+
+
+const RouterComponent = () => {
+    return (
+        <Router navigationBarStyle={styles.navBarStyle}>
+                <Stack key='main' headerTintColor="#c7c7c7">
+                    <Scene
+                        key='Home'
+                        component={Home}
+                        renderLeftButton={<View />}
+                        initial
+                    />
+                </Stack>
+        </Router>
+    )
+}
+
+export default RouterComponent
